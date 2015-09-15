5a4cb6b97bb442c90ded62af6a384d2c3568772d
spaetsch <spaetsch@gmail.com>
2015-09-15 14:10:30 -0700

    First stab at responsiveness for carousel. needs refinement

--- js/main.js
+++ js/main.js
@@ -4,6 +4,27 @@ $(document).ready(function(){
   slidesToShow: 1,
   //autoplay: true,
   dots: true,
-  adaptiveHeight: true,
-  speed: 500 });
+ // adaptiveHeight: true,
+  speed: 500,
+  mobileFirst: true,
+  centerPadding: '1em',
+
+  centerMode: true,
+  responsive: [
+    {
+      breakpoint: '$breakpoint-med',
+      settings: {
+        slidesToShow: 3,
+        centerPadding: '2em'
+      }
+    }
+    // {
+    //   breakpoint: 480,
+    //   settings: {
+
+    //     slidesToShow: 1
+    //   }
+    // }
+  ]
+  });
 });
